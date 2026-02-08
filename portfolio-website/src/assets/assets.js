0892a3bd464095f118ceb74e04dfd1ee1fa7a2f1
iskodemain <francislingan.onlineclass.94@gmail.com>
2026-02-08 23:47:11 +0800

    feat: add required values for displaying 12 work cards

--- portfolio-website/src/assets/assets.js
+++ portfolio-website/src/assets/assets.js
@@ -35,6 +35,12 @@ import ps_6 from './ps_6.png'
 import testimonial_1 from './testimonial_1.png'
 import testimonial_2 from './testimonial_2.png'
 import testimonial_3 from './testimonial_3.png'
+import testimonial_4 from './testimonial_4.png'
+import testimonial_5 from './testimonial_5.png'
+import testimonial_6 from './testimonial_6.png'
+import testimonial_7 from './testimonial_7.png'
+import testimonial_8 from './testimonial_8.png'
+
 
 import about_cover from './about_cover.png'
 
@@ -88,6 +94,232 @@ import seven_dma_8 from './seven_dma_8.jpg'
 
 import one_dma_9 from './one_dma_9.mp4'
 
+
+import asa_1 from './asa_1.png'
+import asa_2 from './asa_2.png'
+import asa_3 from './asa_3.png'
+import asa_4 from './asa_4.png'
+import asa_5 from './asa_5.png'
+import asa_6 from './asa_6.png'
+import asa_7 from './asa_7.png'
+import asa_8 from './asa_8.png'
+import asa_9 from './asa_9.png'
+
+import one_asa_1 from './one_asa_1.mp4'
+import one_asa_2 from './one_asa_2.mp4'
+import one_asa_3 from './one_asa_3.mp4'
+import one_asa_4 from './one_asa_4.mp4'
+import one_asa_5 from './one_asa_5.mp4'
+import one_asa_6 from './one_asa_6.mp4'
+import one_asa_7 from './one_asa_7.mp4'
+import one_asa_8 from './one_asa_8.mp4'
+import one_asa_9 from './one_asa_9.mp4'
+
+
+import sma_1 from './sma_1.png'
+import sma_2 from './sma_2.png'
+import sma_3 from './sma_3.png'
+import sma_4 from './sma_4.png'
+import sma_5 from './sma_5.png'
+import sma_6 from './sma_6.png'
+import sma_7 from './sma_7.png'
+import sma_8 from './sma_8.png'
+import sma_9 from './sma_9.png'
+
+
+import ccp_1 from './ccp_1.png'
+import ccp_2 from './ccp_2.png'
+import ccp_3 from './ccp_3.png'
+import ccp_4 from './ccp_4.png'
+import ccp_5 from './ccp_5.png'
+import ccp_6 from './ccp_6.png'
+import ccp_7 from './ccp_7.png'
+import ccp_8 from './ccp_8.png'
+import ccp_9 from './ccp_9.png'
+
+
+import cs_1 from './cs_1.png'
+import cs_2 from './cs_2.png'
+import cs_3 from './cs_3.png'
+import cs_4 from './cs_4.png'
+import cs_5 from './cs_5.png'
+import cs_6 from './cs_6.png'
+import cs_7 from './cs_7.png'
+import cs_8 from './cs_8.png'
+import cs_9 from './cs_9.png'
+
+
+import pm_1 from './pm_1.png'
+import pm_2 from './pm_2.png'
+import pm_3 from './pm_3.png'
+import pm_4 from './pm_4.png'
+import pm_5 from './pm_5.png'
+import pm_6 from './pm_6.png'
+import pm_7 from './pm_7.png'
+import pm_8 from './pm_8.png'
+import pm_9 from './pm_9.png'
+
+import ndt_1 from './ndt_1.png'
+import ndt_2 from './ndt_2.png'
+import ndt_3 from './ndt_3.png'
+import ndt_4 from './ndt_4.png'
+import ndt_5 from './ndt_5.png'
+import ndt_6 from './ndt_6.png'
+import ndt_7 from './ndt_7.png'
+import ndt_8 from './ndt_8.png'
+import ndt_9 from './ndt_9.png'
+
+import cc_1 from './cc_1.png'
+import cc_2 from './cc_2.png'
+import cc_3 from './cc_3.png'
+
+import mc_1 from './mc_1.png'
+import mc_2 from './mc_2.png'
+import mc_3 from './mc_3.png'
+import mc_4 from './mc_4.png'
+import mc_5 from './mc_5.png'
+import mc_6 from './mc_6.png'
+
+import cga_1 from './cga_1.png'
+import cga_2 from './cga_2.png'
+import cga_3 from './cga_3.png'
+import cga_4 from './cga_4.png'
+import cga_5 from './cga_5.png'
+import cga_6 from './cga_6.png'
+import cga_7 from './cga_7.png'
+import cga_8 from './cga_8.png'
+import cga_9 from './cga_9.png'
+
+import mkt_1 from './mkt_1.mp4'
+
+import dmp_1 from './dmp_1.png'
+import dmp_2 from './dmp_2.png'
+import dmp_3 from './dmp_3.png'
+
+import one_sma_1 from './one_sma_1.png'
+import one_sma_2 from './one_sma_2.mp4'
+
+import one_sma_3 from './one_sma_3.png'
+import two_sma_3 from './two_sma_3.png'
+import three_sma_3 from './three_sma_3.png'
+import four_sma_3 from './four_sma_3.png'
+import five_sma_3 from './five_sma_3.png'
+
+import one_sma_4 from './one_sma_4.png'
+import one_sma_5 from './one_sma_5.mp4'
+import one_sma_6 from './one_sma_6.png'
+import one_sma_7 from './one_sma_7.png'
+
+import one_sma_8 from './one_sma_8.png'
+import two_sma_8 from './two_sma_8.png'
+import three_sma_8 from './three_sma_8.png'
+
+import one_sma_9 from './one_sma_9.png'
+
+import one_ccp_1 from './one_ccp_1.mp4'
+import one_ccp_2 from './one_ccp_2.mp4'
+import one_ccp_3 from './one_ccp_3.mp4'
+import one_ccp_4 from './one_ccp_4.mp4'
+import one_ccp_5 from './one_ccp_5.mp4'
+import one_ccp_6 from './one_ccp_6.mp4'
+import one_ccp_7 from './one_ccp_7.mp4'
+import one_ccp_8 from './one_ccp_8.mp4'
+import one_ccp_9 from './one_ccp_9.mp4'
+
+import one_cs_1 from './one_cs_1.png'
+import one_cs_2 from './one_cs_2.png'
+import one_cs_3 from './one_cs_3.png'
+import one_cs_4 from './one_cs_4.png'
+import one_cs_5 from './one_cs_5.png'
+import one_cs_6 from './one_cs_6.png'
+import one_cs_7 from './one_cs_7.mp4'
+import one_cs_8 from './one_cs_8.mp4'
+
+import one_cs_9 from './one_cs_9.png'
+import two_cs_9 from './two_cs_9.png'
+
+import one_pm_1 from './one_pm_1.png'
+import two_pm_1 from './two_pm_1.png'
+
+import one_pm_2 from './one_pm_2.mp4'
+
+import one_pm_3 from './one_pm_3.png'
+import two_pm_3 from './two_pm_3.png'
+import three_pm_3 from './three_pm_3.png'
+import four_pm_3 from './four_pm_3.png'
+import five_pm_3 from './five_pm_3.png'
+
+import one_pm_4 from './one_pm_4.png'
+import two_pm_4 from './two_pm_4.png'
+import three_pm_4 from './three_pm_4.png'
+
+import one_pm_5 from './one_pm_5.mp4'
+import one_pm_6 from './one_pm_6.mp4'
+
+import one_pm_7 from './one_pm_7.png'
+import two_pm_7 from './two_pm_7.png'
+
+import one_pm_8 from './one_pm_8.png'
+import two_pm_8 from './two_pm_8.png'
+import three_pm_8 from './three_pm_8.png'
+import four_pm_8 from './four_pm_8.png'
+import five_pm_8 from './five_pm_8.png'
+
+import one_pm_9 from './one_pm_9.png'
+
+import vr_1 from './vr_1.mp4'
+
+import one_ndt_1 from './one_ndt_1.png'
+import one_ndt_2 from './one_ndt_2.png'
+
+import one_ndt_3 from './one_ndt_3.png'
+import two_ndt_3 from './two_ndt_3.png'
+import three_ndt_3 from './three_ndt_3.png'
+import four_ndt_3 from './four_ndt_3.png'
+import five_ndt_3 from './five_ndt_3.png'
+import six_ndt_3 from './six_ndt_3.png'
+import seven_ndt_3 from './seven_ndt_3.png'
+
+import one_ndt_4 from './one_ndt_4.png'
+import one_ndt_5 from './one_ndt_5.png'
+import one_ndt_6 from './one_ndt_6.png'
+
+import one_ndt_7 from './one_ndt_7.png'
+import two_ndt_7 from './two_ndt_7.png'
+import three_ndt_7 from './three_ndt_7.png'
+import four_ndt_7 from './four_ndt_7.png'
+import five_ndt_7 from './five_ndt_7.png'
+
+import one_ndt_8 from './one_ndt_8.png'
+import two_ndt_8 from './two_ndt_8.png'
+
+import one_ndt_9 from './one_ndt_9.png'
+
+import one_cc_1 from './one_cc_1.mp4'
+import one_cc_2 from './one_cc_2.mp4'
+import one_cc_3 from './one_cc_3.mp4'
+
+import one_mc_1 from './one_mc_1.mp4'
+import one_mc_2 from './one_mc_2.mp4'
+import one_mc_3 from './one_mc_3.mp4'
+import one_mc_4 from './one_mc_4.mp4'
+import one_mc_5 from './one_mc_5.mp4'
+import one_mc_6 from './one_mc_6.mp4'
+
+import one_dmp_1 from './one_dmp_1.png'
+import one_dmp_2 from './one_dmp_2.png'
+import one_dmp_3 from './one_dmp_3.png'
+
+import one_cga_1 from './one_cga_1.mp4'
+import one_cga_2 from './one_cga_2.mp4'
+import one_cga_3 from './one_cga_3.mp4'
+import one_cga_4 from './one_cga_4.mp4'
+import one_cga_5 from './one_cga_5.mp4'
+import one_cga_6 from './one_cga_6.mp4'
+import one_cga_7 from './one_cga_7.mp4'
+import one_cga_8 from './one_cga_8.mp4'
+import one_cga_9 from './one_cga_9.mp4'
+
 export const assets = {
     logo,
     profile,
@@ -118,6 +350,11 @@ export const assets = {
     testimonial_1,
     testimonial_2,
     testimonial_3,
+    testimonial_4,
+    testimonial_5,
+    testimonial_6,
+    testimonial_7,
+    testimonial_8,
     about_cover,
     wcp_icon,
     ecp_icon,
@@ -158,5 +395,201 @@ export const assets = {
     six_dma_8,
     seven_dma_8,
     one_dma_9,
-    personality_3
+    personality_3,
+    asa_1,
+    asa_2,
+    asa_3,
+    asa_4,
+    asa_5,
+    asa_6,
+    asa_7,
+    asa_8,
+    asa_9,
+    sma_1,
+    sma_2,
+    sma_3,
+    sma_4,
+    sma_5,
+    sma_6,
+    sma_7,
+    sma_8,
+    sma_9,
+    ccp_1,
+    ccp_2,
+    ccp_3,
+    ccp_4,
+    ccp_5,
+    ccp_6,
+    ccp_7,
+    ccp_8,
+    ccp_9,
+    cs_1,
+    cs_2,
+    cs_3,
+    cs_4,
+    cs_5,
+    cs_6,
+    cs_7,
+    cs_8,
+    cs_9,
+    pm_1,
+    pm_2,
+    pm_3,
+    pm_4,
+    pm_5,
+    pm_6,
+    pm_7,
+    pm_8,
+    pm_9,
+    ndt_1,
+    ndt_2,
+    ndt_3,
+    ndt_4,
+    ndt_5,
+    ndt_6,
+    ndt_7,
+    ndt_8,
+    ndt_9,
+    cc_1,
+    cc_2,
+    cc_3,
+    mc_1,
+    mc_2,
+    mc_3,
+    mc_4,
+    mc_5,
+    mc_6,
+    cga_1,
+    cga_2,
+    cga_3,
+    cga_4,
+    cga_5,
+    cga_6,
+    cga_7,
+    cga_8,
+    cga_9,
+    mkt_1,
+    dmp_1,
+    dmp_2,
+    dmp_3,
+    one_asa_1,
+    one_asa_2,
+    one_asa_3,
+    one_asa_4,
+    one_asa_5,
+    one_asa_6,
+    one_asa_7,
+    one_asa_8,
+    one_asa_9,
+
+    one_sma_1,
+    one_sma_2,
+
+    one_sma_3,
+    two_sma_3,
+    three_sma_3,
+    four_sma_3,
+    five_sma_3,
+
+    one_sma_4,
+    one_sma_5,
+    one_sma_6,
+    one_sma_7,
+
+    one_sma_8,
+    two_sma_8,
+    three_sma_8,
+    
+    one_sma_9,
+
+    one_ccp_1,
+    one_ccp_2,
+    one_ccp_3,
+    one_ccp_4,
+    one_ccp_5,
+    one_ccp_6,
+    one_ccp_7,
+    one_ccp_8,
+    one_ccp_9,
+
+    one_cs_1,
+    one_cs_2,
+    one_cs_3,
+    one_cs_4,
+    one_cs_5,
+    one_cs_6,
+    one_cs_7,
+    one_cs_8,
+    one_cs_9,
+    two_cs_9,
+
+    one_pm_1,
+    two_pm_1,
+    one_pm_2,
+    one_pm_3,
+    two_pm_3,
+    three_pm_3,
+    four_pm_3,
+    five_pm_3,
+    one_pm_4,
+    two_pm_4,
+    three_pm_4,
+    one_pm_5,
+    one_pm_6,
+    one_pm_7,
+    two_pm_7,
+    one_pm_8,
+    two_pm_8,
+    three_pm_8,
+    four_pm_8,
+    five_pm_8,
+    one_pm_9,
+    vr_1,
+
+    one_ndt_1,
+    one_ndt_2,
+    one_ndt_3,
+    two_ndt_3,
+    three_ndt_3,
+    four_ndt_3,
+    five_ndt_3,
+    six_ndt_3,
+    seven_ndt_3,
+    one_ndt_4,
+    one_ndt_5,
+    one_ndt_6,
+    one_ndt_7,
+    two_ndt_7,
+    three_ndt_7,
+    four_ndt_7,
+    five_ndt_7,
+    one_ndt_8,
+    two_ndt_8,
+    one_ndt_9,
+
+    one_cc_1,
+    one_cc_2,
+    one_cc_3,
+
+    one_mc_1,
+    one_mc_2,
+    one_mc_3,
+    one_mc_4,
+    one_mc_5,
+    one_mc_6,
+
+    one_dmp_1,
+    one_dmp_2,
+    one_dmp_3,
+
+    one_cga_1,
+    one_cga_2,
+    one_cga_3,
+    one_cga_4,
+    one_cga_5,
+    one_cga_6,
+    one_cga_7,
+    one_cga_8,
+    one_cga_9,
+
 }
